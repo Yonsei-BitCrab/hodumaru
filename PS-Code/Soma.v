2021283c079466e32493e3ed701db5cf2e073abc
BitCrab-Yonsei <bitcrab.yonsei@gmail.com>
2023-07-22 14:09:05 +0900

    temp save-can synt
    Co-authored-by: 백세인 <shanebaik@o365.yonsei.ac.kr>
    Co-authored-by: 이주영 <lljad9@o365.yonsei.ac.kr>

--- PS-Code/Soma.v
+++ PS-Code/Soma.v
@@ -1,42 +1,43 @@
 // soma module in physical neuron
 
 module soma (	clk, rst, kill,
-				vrest, vth, vlk, r_time, a_delay, 
+				V_rest, V_th, V_leak, refr_time, axon_delay, 
 				weight,
 			 	in_spike, 
-				suspend,
+				o_wait,
 				out_spike);
   
 // Neuron Inputs Declaration
 input wire clk;
 input wire [7:0] in_spike; //in
 input wire [7:0] weight; //TODO:
+input rst, kill;
 
 // Neuron parameter data to be Initialized
-input wire [7:0] vrest;
-input wire [7:0] vth;
-input wire [7:0] vlk;
-input wire [7:0] r_time;
-input wire [7:0] a_delay;
+input wire [7:0] V_rest;
+input wire [7:0] V_th;
+input wire [7:0] V_leak;
+input wire [7:0] refr_time;
+input wire [7:0] axon_delay;
 
 //Neuron Outputs Declaration
 
-wire w_suspend;
-output suspend;
+reg _wait;
+output o_wait;
 output out_spike;
 
 // Spike Time
 integer _spike;
-integer _spike2Accumulate;
+integer _spikeDelaySum;
 
   
 // Neuron Constants
-reg[7:0] _vrest;
-reg[7:0] _vth;
-reg[7:0] _r_time;
-reg[7:0] _a_delay;
-reg[7:0] _vlk;
-reg[] _V_potential;
+reg[7:0] _V_rest;
+reg[7:0] _V_th;
+reg[7:0] _refr_time;
+reg[7:0] _axon_delay;
+reg[7:0] _V_leak;
+integer _V_potential;			//TODO: change w/ fixed point library
 
 
 reg _is_REF;
@@ -46,7 +47,7 @@ reg[1:0] state_reg, next_state_reg;
 
 parameter DEACTIVE = 2'b00;
 parameter ACTIVE = 2'b01;
-parameter REFRATORY = 2'b10;
+parameter REFRATORY = 2'b11;
 
 // Neural Type Initializing using FSM
 always @(posedge clk or negedge rst) begin
@@ -65,7 +66,7 @@ always @(posedge clk or negedge rst) begin
 				if (kill == 1'b1) begin
 					next_state_reg <= DEACTIVE; 
 				end
-				else if (_is_REF) begin
+				else if (_is_REF == 1'b1) begin
 					next_state_reg <= REFRATORY;
 				end
 				else begin
@@ -73,12 +74,18 @@ always @(posedge clk or negedge rst) begin
 				end    
 			
 			REFRATORY:
-				if (!_is_REF) begin
+				if (kill == 1'b1) begin
+					next_state_reg <= DEACTIVE;
+				end
+				else if (_is_REF == 1'b0) begin
 					next_state_reg <= ACTIVE;
 				end
 				else begin
 					next_state_reg <= next_state_reg;
 				end
+				
+			default:
+				next_state_reg <= next_state_reg; 
 		endcase
 	end
 end
@@ -87,41 +94,55 @@ end
 // Neuron Dynamics
 always @(posedge clk or negedge rst) begin 
 	if (!rst) begin
-		_vrest <= vrest;
-		_V_potential <= vrest;
-		_vth <= vth;
-		_r_time <= r_time;
-		_a_delay <= a_delay;
-		_vlk <= vlk;
+		_V_rest <= V_rest;
+		_V_potential <= V_rest;
+		_V_th <= V_th;
+		_refr_time <= refr_time;
+		_axon_delay <= axon_delay;
+		_V_leak <= V_leak;
 	end
 
-	if (state_reg == ACTIVE) begin
-		_V_potential <= _V_potential + weight - _vlk; // TODO: (LLIF equation)  //TODO: int() wrap!
+	else if (state_reg == ACTIVE) begin
+		_V_potential <= _V_potential + weight - in_spike *_V_leak; // TODO: (LLIF equation)
 
-		if (_V_potential >= _vth) begin //fire out_spike
-			_V_potential <= _vrest;
-			_spike <= in_spike + _a_delay;
+		//_V_potential <= _V_potential + del_t / time_constant ( _V_rest - _V_potential + weight )
+
+		if (_V_potential >= _V_th) begin //fire out_spike
+			_V_potential <= _V_rest;
+			_spike <= in_spike + _axon_delay;
 			_is_REF <= 1'b1;
 			
 			
 		end
 
-		if (_V_potential < _vrest) begin //return to V_rest
-			_V_potential <= _vrest;
+		else if (_V_potential < _V_rest) begin //return to V_rest
+			_V_potential <= _V_rest;
 		end
 
 	end
     
 	else if (state_reg == REFRATORY) begin
-		_spike2Accumulate <= _spike2Accumulate + in_spike;	//TODO: int() wrap!
-		if (_spike2Accumulate > _r_time) begin
+		_spikeDelaySum <= _spikeDelaySum + in_spike;
+		if (_spikeDelaySum >= _refr_time) begin
+			_spikeDelaySum <= 0;
 			_is_REF <= 1'b0;
-			w_suspend <= 1'b1;		//TODO: grammar check needed
+			_wait <= 1'b1;		//TODO: grammar check needed
 		end
+		else begin
+			_is_REF <= 1'b1;
+			_wait <= 1'b0;
+		end
+
+	end
+
+	else begin
+		_wait <= 1'b0;
+		_is_REF <= 1'b0;
+		_spikeDelaySum <= 0;
 	end
 end
 
-assign suspend = w_suspend;
+assign o_wait = _wait;
 assign output_spike = _spike;
 
 endmodule

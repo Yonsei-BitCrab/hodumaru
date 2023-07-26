3e72cb4a85fcbc8aa52e3934ad8ed3f2b501e302
seinbaik <shanebaik1@gmail.com>
2023-07-26 21:23:00 +0900

    fuck... 09:22pm 0726

--- PS-Code/Synapse.v
+++ PS-Code/Synapse.v
@@ -17,20 +17,20 @@ input W_EN, R_EN;
 output reg [7:0] _weight_out;
 
 reg [15:0] _iADDR;
-reg [31:0] _W_DATA;
+//reg [31:0] _W_DATA;
 reg [6:0] _iAddr;
-reg [31:0] _Weight_table [0:31] ; //memory
+reg [31:0] _Weight_table [0:31]; //memory
 reg _count = 0;
 //reg [7:0] __weight_out;
 reg [4:0] _base; //TODO: int wrap;
 reg [1:0]_remains;
 
 
-
 always @(posedge clk or negedge rst) begin 
+
 	if (!rst) begin // init : #table 저장
         if (W_EN == 1'b1) begin
-        _Weight_table[_count] <= _W_DATA;
+        _Weight_table[_count] <= W_DATA;
         _count <= _count + 1;
         end
 
@@ -76,31 +76,30 @@ always @(posedge clk or negedge rst) begin
             
             // _W_DATA slicing needed into 1B like below...
             if (_remains ==2'b00)begin
-            _Weight_table[_base][7:0] <= _W_DATA[7:0]; //TODO: 접근방법
+            _Weight_table[_base][7:0] <= W_DATA[7:0]; //TODO: 접근방법
             end
 
             else if(_remains ==2'b01)begin
-            _Weight_table[_base][15:8] <= _W_DATA[7:0]; //TODO: 접근방법
+            _Weight_table[_base][15:8] <= W_DATA[7:0]; //TODO: 접근방법
             end
 
             else if(_remains ==2'b10)begin
-           _Weight_table[_base][23:16] <= _W_DATA[7:0]; //TODO: 접근방법
+           _Weight_table[_base][23:16] <= W_DATA[7:0]; //TODO: 접근방법
             end
             
             else begin
-            _Weight_table[_base][31:24] <= _W_DATA[7:0]; //TODO: 접근방법
+            _Weight_table[_base][31:24] <= W_DATA[7:0]; //TODO: 접근방법
             end
             
         end
 
         else begin
             _weight_out <= _weight_out; // or _weight_out <= 0;
-            _W_DATA <= _W_DATA;
-            _Weight_table <= _Weight_table;
         end
 
 
     end
+
 
 // assign _weight_out = __weight_out;
 

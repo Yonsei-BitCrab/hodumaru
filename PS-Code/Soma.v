25a29890a72ad60ff8fe7875696647de211e8aca
seinbaik <shanebaik1@gmail.com>
2023-07-30 19:47:41 +0900

    holy!

--- PS-Code/Soma.v
+++ PS-Code/Soma.v
@@ -8,7 +8,7 @@
 		input [31:0] W_DATA, //from PN controller
 		input [15:0] weight, //from synapse
 		output o_wait,
-		output reg [15:0] spike_out,
+		output reg [15:0] spike_out
 
 	);
 	
@@ -27,8 +27,9 @@
 	reg[7:0] _refr_time;
 	reg[15:0] _axon_delay = 0;
 	reg[15:0] _spike_interval;
-	reg[15:0] _V_potential;			//TODO: change w/ fixed point library
-	reg _is_REF;
+	reg[15:0] _V_potential;
+	reg[15:0] _dummy;			//TODO: change w/ fixed point library
+	wire _is_REF; //TODO:
 	integer tau = 10;
 
 
@@ -41,6 +42,21 @@
 	parameter _E = 2;
 
 
+	check_isREF CIR (
+		._V_potential(_V_potential),
+		._spike_interval(_spike_interval),
+		._axon_delay(_axon_delay),
+		._V_th(_V_th),
+		.spike_out(spike_out),
+		._is_REF(_is_REF)
+	);
+
+	confirm_isREF COIR (
+		._spikeDelaySum_int(_spikeDelaySum[15:8]),
+		._refr_time(_refr_time),
+		._is_REF(_is_REF)		
+	);
+	
 	// Neural Type Initializing using FSM
 	always @(posedge clk or negedge rst) begin
 		
@@ -53,7 +69,6 @@
 		end
 
 		else begin
-//			state_reg <= state_reg;
 			case (state_reg)
 				DEACTIVE:
 					if(en) begin
@@ -112,63 +127,88 @@
 		else begin
 
 		if (state_reg == ACTIVE) begin
-			//_V_potential <= _V_potential + weight - in_spike *_V_leak; // TODO: (LLIF equation)
-
-			//_V_potential <= _V_potential + del_t / time_constant ( _V_rest - _V_potential + weight )
-			// _V_potential <= _V_potential * (1-e**(_spike_interval/tau)) + weight; //TODO: LIF, e quatization sub
-			_V_potential <= _V_potential * (1-_E**(_spike_interval/tau)) + weight; //TODO: LIF, e quatization sub
-
-			if (_V_potential >= _V_th) begin //fire out_spike
-				_V_potential <= 0;
-				spike_out <= _spike_interval + _axon_delay; //fire spike
-				_is_REF <= 1'b1;
+			if(!_is_REF) begin
+				_V_potential <= _V_potential * (1-_E**(_spike_interval/tau)) + weight; //TODO: LIF, e quatization sub
+				_dummy <= 0;
 			end
 
 			else begin
-			_V_potential <= _V_potential;
-			_V_th <= _V_th;
-			_refr_time <= _refr_time;
-			_axon_delay <= _axon_delay;
-			_V_leak <= _V_leak;
+				_V_potential <= 0;
+				_dummy <= weight;
+				_dummy <= _spike_interval;
 			end
-			
-
 		end
 		
 		else if (state_reg == REFRATORY) begin
-			_spikeDelaySum <= _spikeDelaySum + _spike_interval;
-			if (_spikeDelaySum >= _refr_time) begin
+			if (!_is_REF) begin
 				_spikeDelaySum <= 0;
-				_is_REF <= 1'b0;
-				_wait <= 1'b1;		//TODO: grammar check needed
 			end
-			
+
 			else begin
-				_is_REF <= 1'b1;
-				_wait <= 1'b0;
-				_V_potential <= _V_potential;
-				_V_th <= _V_th;
-				_refr_time <= _refr_time;
-				_axon_delay <= _axon_delay;
-				_V_leak <= _V_leak;
+				_spikeDelaySum <= _spikeDelaySum + _spike_interval;
 			end
 
 		end
 
 		else begin
-			_wait <= 1'b0;
-			_is_REF <= 1'b0;
-			_spikeDelaySum <= 0;
-
-			_V_potential <= _V_potential;
 			_V_th <= _V_th;
+			_V_leak <= _V_leak;
 			_refr_time <= _refr_time;
 			_axon_delay <= _axon_delay;
-			_V_leak <= _V_leak;
+			_V_potential <= _V_potential;
+			_spikeDelaySum <= _spikeDelaySum;
 		end
 	end
 	end
-	
+
 	assign o_wait = _wait;
 
+	endmodule
+
+
+
+
+
+///////////////////////////////
+///////////////////////////////
+
+	module check_isREF (
+		input [15:0] _V_potential,
+		input [15:0] _spike_interval,
+		input [15:0] _axon_delay,
+		input [7:0] _V_th,
+		output reg [15:0] spike_out,
+		output reg _is_REF
+	);
+
+
+	always @(_V_potential) begin
+		if (_V_potential >= _V_th) begin
+			_is_REF <= 1'b1;
+			spike_out <= _spike_interval + _axon_delay; //fire spike
+			_spikeDelaySum <= 0;
+			end
+		else begin
+			_is_REF <= 1'b0;
+			spike_out <= 0;
+		end
+	end
+	
+	endmodule
+
+
+	module confirm_isREF (
+		input [7:0] _spikeDelaySum_int;
+		input [7:0] _refr_time;
+		output reg _is_REF
+	);
+		always @(_spikeDelaySum_int) begin
+			if (_spikeDelaySum_int >= _refr_time) begin
+				_is_REF <= 1'b0;
+			end
+			else begin
+				_is_REF <= 1'b1;
+			end
+				//_wait <= 1'b1;		//TODO: grammar check needed
+		end
 	endmodule

4560df739d8f393f8fbc7ce1ef78d16ca06133b9
seinbaik <shanebaik1@gmail.com>
2023-08-02 20:52:24 +0900

    yaho

---
+++ PS-Code/PhysicalNeuronController/test/test_PNC_top.v
@@ -0,0 +1,134 @@
+module PhysicalNeuronController_iLoop(
+    clk, rst, kill,
+    iADDR, W_DATA,
+    SWU_EN, SWU_ADDR, SWU_DATA,
+
+    Addr2SYNAPSE, Addr2SOMA, Addr2STDP,
+    Data2SYNAPSE, Data2SOMA, Data2STDP,
+
+    EN_SYNAPSE, EN_SOMA, EN_STDP,
+    RC_SYNAPSE, RC_SOMA, RC_STDP,
+    W_EN2Synapse, W_EN2SOMA, W_EN2STDP,
+    o_ctrl, dbg_DATA1, dbg_DATA2
+);
+
+input clk, rst, kill;
+input [15:0] iADDR;
+input [31:0] W_DATA;
+
+input SWU_EN;
+input [15:0] SWU_ADDR;
+input [7:0] SWU_DATA;
+
+output [6:0] Addr2SYNAPSE, Addr2SOMA, Addr2STDP;
+output [31:0] Data2SYNAPSE, Data2SOMA, Data2STDP;
+
+output EN_SYNAPSE, EN_SOMA, EN_STDP;
+output RC_SYNAPSE, RC_SOMA, RC_STDP;
+output W_EN2Synapse, W_EN2SOMA, W_EN2STDP;
+
+// DEBUG
+output [1:0] o_ctrl;
+output [31:0] dbg_DATA1, dbg_DATA2;
+
+// Stack Machine
+wire [15:0] STMC_gate_ADDR_Din;
+wire [15:0] STMC_gate_ADDR_Dout;
+//reg [1:0] STMC_gate_ADDR_ctl_in;
+wire STMC_gate_ADDR_wait_out;
+
+wire [31:0] STMC_gate_DATA_Din;
+wire [31:0] STMC_gate_DATA_Dout;
+//reg [1:0] STMC_gate_DATA_ctl_in;
+wire STMC_gate_DATA_wait_out;
+
+
+//DEBUG ///
+// assign STMC_gate_ADDR_Din = SWU_EN ? SWU_ADDR : iADDR; //Origin Code
+// assign STMC_gate_DATA_Din = SWU_EN ? {24'b0, SWU_DATA} : W_DATA;  // Origin Code
+//always @(posedge clk) begin
+assign STMC_gate_ADDR_Din = SWU_EN ? SWU_ADDR : iADDR; //Origin Code
+assign STMC_gate_DATA_Din = SWU_EN ? {24'b0, SWU_DATA} : W_DATA;  // Origin Code
+//end
+//DEBUG ///
+
+
+// TODO: Control line for STMC is needed
+wire [1:0] ctl_line;
+PNC_STMC_Control_Unit STMC_CU (
+    .clk(clk),
+	.rst(rst),
+    .iAddr(STMC_gate_ADDR_Din),
+    .ctrl(ctl_line)
+);
+
+//DEBUG
+assign o_ctrl = ctl_line;
+//assign STMC_gate_ADDR_ctl_in = ctl_line;
+//assign STMC_gate_DATA_ctl_in = ctl_line;
+
+
+STACK_MACHINE_ADDR STMC_gate_ADDR (
+    .clk(clk),
+    .rst(kill),
+    //.ctl(STMC_gate_ADDR_ctl_in),
+	 .ctl(ctl_line),
+    .o_wait(STMC_gate_ADDR_wait_out),
+    .DATA_in(STMC_gate_ADDR_Din),
+    .DATA_out(STMC_gate_ADDR_Dout)
+);
+
+STACK_MACHINE_DATA STMC_gate_DATA  (
+    .clk(clk),
+    .rst(kill),
+    //.ctl(STMC_gate_DATA_ctl_in),
+	.ctl(ctl_line),
+    .o_wait(STMC_gate_DATA_wait_out),
+    .DATA_in(STMC_gate_DATA_Din),
+    .DATA_out(STMC_gate_DATA_Dout)
+);
+
+// debUG
+assign dbg_DATA1 = STMC_gate_DATA_Din;
+assign dbg_DATA2 = STMC_gate_DATA_Dout;
+
+// Control Unit
+wire w_EN_SYNAPSE, w_EN_SOMA, w_EN_STDP;
+wire w_RC_SYNAPSE, w_RC_SOMA, w_RC_STDP;
+wire w_W_EN2Synapse, w_W_EN2SOMA, w_W_EN2STDP;
+
+PNC_ADDR_Control_Unit PNC_ADDR_CU(
+    .clk(clk),
+    .iADDR_ctl(STMC_gate_ADDR_Dout[15:12]),
+    .EN_SYNAPSE(w_EN_SYNAPSE), 
+    .EN_SOMA(w_EN_SOMA), 
+    .EN_STDP(w_EN_STDP),
+    .RC_SYNAPSE(w_RC_SYNAPSE), 
+    .RC_SOMA(w_RC_SOMA), 
+    .RC_STDP(w_RC_STDP),
+    .W_EN2Synapse(w_W_EN2Synapse), 
+    .W_EN2SOMA(w_W_EN2SOMA), 
+    .W_EN2STDP(w_W_EN2STDP)
+);
+
+// ADDR & DATA MUX
+assign Addr2SYNAPSE = w_EN_SYNAPSE ? STMC_gate_ADDR_Dout[6:0] : 0;
+assign Addr2SOMA = w_EN_SOMA ? STMC_gate_ADDR_Dout[6:0] : 0;
+assign Addr2STDP = w_EN_STDP ? STMC_gate_ADDR_Dout[6:0] : 0;
+assign Data2SYNAPSE = w_EN_SYNAPSE ? STMC_gate_DATA_Dout : 0;
+assign Data2SOMA = w_EN_SOMA ? STMC_gate_DATA_Dout : 0;
+assign Data2STDP = w_EN_STDP ? STMC_gate_DATA_Dout : 0;
+
+// Control lines
+assign EN_SYNAPSE = w_EN_SYNAPSE;
+assign EN_SOMA = w_EN_SOMA;
+assign EN_STDP = w_EN_STDP;
+assign RC_SYNAPSE = w_RC_SYNAPSE;
+assign RC_SOMA = w_RC_SOMA;
+assign RC_STDP = w_RC_STDP;
+assign W_EN2Synapse = w_W_EN2Synapse;
+assign W_EN2SOMA = w_W_EN2SOMA;
+assign W_EN2STDP = w_W_EN2STDP;
+
+
+endmodule
---
+++ PS-Code/PhysicalNeuronController/test/test_STMC_Addr.v
@@ -0,0 +1,300 @@
+module STACK_MACHINE_ADDR ( clk, rst,
+						ctl, o_wait,
+						DATA_in, DATA_out
+                        // o_STACK_REG0, o_STACK_REG[1], o_STACK_REG[2]
+);
+
+parameter DATA_WIDTH = 16;
+parameter STACK_SIZE = 3;       // IF Size increases, You have to add codes(refer to STATE REG)
+
+input clk, rst;
+input [1:0] ctl;
+input [DATA_WIDTH-1:0] DATA_in;
+output reg o_wait;
+output [DATA_WIDTH-1:0] DATA_out;
+// output [15:0]  o_STACK_REG0, o_STACK_REG[1], o_STACK_REG[2];
+
+reg [1:0] next_state_reg, state_reg;
+reg[DATA_WIDTH-1:0] _STACK_REG[STACK_SIZE-1:0];
+reg[DATA_WIDTH-1:0] _next_STACK_REG[STACK_SIZE-1:0];
+// reg[15:0] o_STACK_REG0, o_STACK_REG[1], o_STACK_REG[2];
+reg [DATA_WIDTH-1:0] buf_DATA_out;
+
+always @(posedge clk) begin
+	if (rst == 1'b1) begin
+		state_reg <= 0;
+	end
+	else begin
+		state_reg <= next_state_reg;
+	end
+end
+
+always @(posedge clk) begin
+    if (rst == 1'b1)  begin
+        _STACK_REG[0] <= 0;
+        _STACK_REG[1] <= 0;
+        _STACK_REG[2] <= 0;
+    end
+    else begin
+        _STACK_REG[0] <= _next_STACK_REG[0];
+        _STACK_REG[1] <= _next_STACK_REG[1];
+        _STACK_REG[2] <= _next_STACK_REG[2];
+    end
+end
+
+
+always @(posedge clk) begin
+	if (rst == 1'b1) begin
+		next_state_reg <= 0;
+	end
+	else begin
+		case(state_reg)
+			2'b00:
+				case (ctl)
+				2'b00:
+					next_state_reg <= 2'b00;
+				2'b01:
+					next_state_reg <= 2'b00;
+				2'b10:
+					next_state_reg <= 2'b00;
+				2'b11:
+					next_state_reg <= 2'b01;
+				default:
+					next_state_reg <= 2'b00;
+				endcase
+				
+			2'b01:
+				case (ctl)
+				2'b00:
+					next_state_reg <= 2'b00;
+				2'b01:
+					next_state_reg <= 2'b01;
+				2'b10:
+					next_state_reg <= 2'b01;
+				2'b11:
+					next_state_reg <= 2'b10;
+				default:
+					next_state_reg <= 2'b00;
+				endcase
+
+			2'b10:
+				case (ctl)
+				2'b00:
+					next_state_reg <= 2'b01;
+				2'b01:
+					next_state_reg <= 2'b10;
+				2'b10:
+					next_state_reg <= 2'b10;
+				2'b11:
+					next_state_reg <= 2'b11;
+				default:
+					next_state_reg <= 2'b00;
+				endcase
+				
+			2'b11:
+				case (ctl)
+				2'b00:
+					next_state_reg <= 2'b10;
+				2'b01:
+					next_state_reg <= 2'b11;
+				2'b10:
+					next_state_reg <= 2'b11;
+				2'b11:
+					next_state_reg <= 2'b10;
+				default:
+					next_state_reg <= 2'b00;
+				endcase
+				
+			default:
+				next_state_reg <= next_state_reg;
+		endcase
+	end
+end
+
+always @(posedge clk) begin
+	if (rst == 1'b1) begin
+        buf_DATA_out <= 0;
+		o_wait <= 1'b0;
+        _next_STACK_REG[0] <= 0;
+        _next_STACK_REG[1] <= 0;
+        _next_STACK_REG[2] <= 0;
+	end
+	else begin
+		case (state_reg)
+		2'b00:
+			case (ctl)
+			2'b00:
+            begin
+                buf_DATA_out <= _STACK_REG[0];
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= 0;
+                _next_STACK_REG[1] <= 0;
+                _next_STACK_REG[2] <= 0;
+            end
+				
+			2'b01:
+            begin
+                buf_DATA_out <= {DATA_in[DATA_WIDTH-1: DATA_WIDTH-4], {(DATA_WIDTH/2-4){1'b0}}, DATA_in[DATA_WIDTH/2-1:0]};
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= 0;
+                _next_STACK_REG[1] <= 0;
+                _next_STACK_REG[2] <= 0;
+            end
+				
+			2'b10:
+            begin
+                buf_DATA_out <= {DATA_in[DATA_WIDTH-1: DATA_WIDTH-4], {(DATA_WIDTH/2-4){1'b0}}, DATA_in[DATA_WIDTH/2-1:0]};
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= 0;
+                _next_STACK_REG[1] <= 0;
+                _next_STACK_REG[2] <= 0;
+            end
+				
+			2'b11:
+            begin
+                buf_DATA_out <= {DATA_in[DATA_WIDTH-1: DATA_WIDTH-4], {(DATA_WIDTH/2-4){1'b0}}, DATA_in[(DATA_WIDTH/2)-1:0]};
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= {DATA_in[DATA_WIDTH-1: DATA_WIDTH-4], {(DATA_WIDTH/2-4){1'b0}}, DATA_in[DATA_WIDTH-1:(DATA_WIDTH/2)]};
+                _next_STACK_REG[1] <= 0;
+                _next_STACK_REG[2] <= 0;
+            end
+				
+				
+			endcase
+		2'b01:
+			case (ctl)
+			2'b00:
+            begin
+                buf_DATA_out <= _STACK_REG[0];
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= 0;
+                _next_STACK_REG[1] <= 0;
+                _next_STACK_REG[2] <= 0;
+            end
+                
+			2'b01:
+            begin
+                buf_DATA_out <= _STACK_REG[0];
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= {DATA_in[DATA_WIDTH-1: DATA_WIDTH-4], {(DATA_WIDTH/2-4){1'b0}}, DATA_in[(DATA_WIDTH/2)-1:0]};
+                _next_STACK_REG[1] <= 0;
+                _next_STACK_REG[2] <= 0;
+            end
+                
+			2'b10:
+            begin
+                buf_DATA_out <= _STACK_REG[0];
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= {DATA_in[DATA_WIDTH-1: DATA_WIDTH-4], {(DATA_WIDTH/2-4){1'b0}}, DATA_in[(DATA_WIDTH/2)-1:0]};
+                _next_STACK_REG[1] <= 0;
+                _next_STACK_REG[2] <= 0;
+            end
+                
+			2'b11:
+            begin
+                buf_DATA_out <= _STACK_REG[0];
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= {DATA_in[DATA_WIDTH-1: DATA_WIDTH-4], {(DATA_WIDTH/2-4){1'b0}}, DATA_in[(DATA_WIDTH/2)-1:0]};
+                _next_STACK_REG[1] <= {DATA_in[DATA_WIDTH-1: DATA_WIDTH-4], {(DATA_WIDTH/2-4){1'b0}}, DATA_in[DATA_WIDTH-1:(DATA_WIDTH/2)]};
+                _next_STACK_REG[2] <= 0;
+            end
+                
+			endcase
+		2'b10:
+			case (ctl)
+			2'b00:
+            begin
+                buf_DATA_out <= _STACK_REG[0];
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= _STACK_REG[1];
+                _next_STACK_REG[1] <= 0;
+                _next_STACK_REG[2] <= 0;
+            end
+                
+			2'b01:
+            begin
+                buf_DATA_out <= _STACK_REG[0];
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= _STACK_REG[1];
+                _next_STACK_REG[1] <= {DATA_in[DATA_WIDTH-1: DATA_WIDTH-4], {(DATA_WIDTH/2-4){1'b0}}, DATA_in[(DATA_WIDTH/2)-1:0]};
+                _next_STACK_REG[2] <= 0;
+            end
+                
+			2'b10:
+            begin
+                buf_DATA_out <= _STACK_REG[0];
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= _STACK_REG[1];
+                _next_STACK_REG[1] <= {DATA_in[DATA_WIDTH-1: DATA_WIDTH-4], {(DATA_WIDTH/2-4){1'b0}}, DATA_in[(DATA_WIDTH/2)-1:0]};
+                _next_STACK_REG[2] <= 0;
+            end
+                
+			2'b11:
+            begin
+                buf_DATA_out <= _STACK_REG[0];
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= _STACK_REG[1];
+                _next_STACK_REG[1] <= {DATA_in[DATA_WIDTH-1: DATA_WIDTH-4], {(DATA_WIDTH/2-4){1'b0}}, DATA_in[(DATA_WIDTH/2)-1:0]};
+                _next_STACK_REG[2] <= {DATA_in[DATA_WIDTH-1: DATA_WIDTH-4], {(DATA_WIDTH/2-4){1'b0}}, DATA_in[DATA_WIDTH-1:(DATA_WIDTH/2)]};
+            end
+                
+			endcase
+		2'b11:
+			case (ctl)
+			2'b00:
+            begin
+                buf_DATA_out <= _STACK_REG[0];
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= _STACK_REG[1];
+                _next_STACK_REG[1] <= _STACK_REG[2];
+                _next_STACK_REG[2] <= 0;
+            end
+                
+			2'b01:
+            begin
+                buf_DATA_out <= _STACK_REG[0];
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= _STACK_REG[1];
+                _next_STACK_REG[1] <= _STACK_REG[2];
+                _next_STACK_REG[2] <= {DATA_in[DATA_WIDTH-1: DATA_WIDTH-4], {(DATA_WIDTH/2-4){1'b0}}, DATA_in[(DATA_WIDTH/2)-1:0]};
+            end
+                
+			2'b10:
+            begin
+                buf_DATA_out <= _STACK_REG[0];
+				o_wait <= 1'b0;
+                _next_STACK_REG[0] <= _STACK_REG[1];
+                _next_STACK_REG[1] <= _STACK_REG[2];
+                _next_STACK_REG[2] <= {DATA_in[DATA_WIDTH-1: DATA_WIDTH-4], {(DATA_WIDTH/2-4){1'b0}}, DATA_in[(DATA_WIDTH/2)-1:0]};
+            end
+                
+			2'b11:
+            begin
+                buf_DATA_out <= _STACK_REG[0];
+				o_wait <= 1'b1;
+                _next_STACK_REG[0] <= _STACK_REG[1];
+                _next_STACK_REG[1] <= _STACK_REG[2];
+                _next_STACK_REG[2] <= 0;
+            end
+                
+			endcase
+		// default:
+        // begin
+        //     buf_DATA_out <= buf_DATA_out;
+		// 	o_wait <= 1'b0;
+        //     _next_STACK_REG[0] <= _next_STACK_REG[1];
+        //     _next_STACK_REG[1] <= _next_STACK_REG[2];
+        //     _next_STACK_REG[2] <= _next_STACK_REG[2];
+        // end
+			
+		endcase
+	end
+end
+
+// DEBUG
+// assign o_STACK_REG[0] = _STACK_REG[0];
+// assign o_STACK_REG[1] = _STACK_REG[1];
+// assign o_STACK_REG[2] = _STACK_REG[2];
+assign DATA_out = buf_DATA_out;
+
+endmodule
